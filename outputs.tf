5489e31e6d2bf505b69f85d46858629b3eb87507
nag9844 <96878041+nag9844@users.noreply.github.com>
2025-06-12 05:17:50 +0000

    update reference values

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,44 @@
 output "vpc_id" {
   description = "VPC ID"
-  value       = aws_vpc.main.id
+  value       = module.vpc.vpc_id
 }
 
-output "public_subnet_ids" {
-  description = "Public subnet IDs"
-  value       = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-}
+# output "public_subnet_ids" {
+#   description = "Public subnet IDs"
+#   value       = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+# }
 
-output "private_subnet_ids" {
-  description = "Private subnet IDs"
-  value       = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-}
+# output "private_subnet_ids" {
+#   description = "Private subnet IDs"
+#   value       = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+# }
 
 output "cluster_name" {
   description = "EKS cluster name"
-  value       = aws_eks_cluster.main.name
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   description = "EKS cluster endpoint"
-  value       = aws_eks_cluster.main.endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "EKS cluster security group ID"
-  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  # value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  value       = module.eks.cluster_security_group_id
 }
 
 output "ecr_repository_url" {
   description = "ECR repository URL"
-  value       = aws_ecr_repository.flask_microservice.repository_url
+  # value       = aws_ecr_repository.flask_microservice.repository_url
+  value       = module.ecr.repository_url
 }
 
 output "kubeconfig_certificate_authority_data" {
   description = "Base64 encoded certificate data"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
+  # value       = aws_eks_cluster.main.certificate_authority[0].data
+  value = module.eks.kubeconfig_certificate_authority_data[0].data
   sensitive   = true
 }
 
